17a0540676e62e1ef594a8ca2b52fc5e55f171bf
Eduardo Riol Fernández <eriol@atsistemas.com>
2018-07-03 10:20:16 +0200

    first commit

---
+++ src/test/java/stepdefinitions/RunCukeTest.java
@@ -0,0 +1,16 @@
+package stepdefinitions;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	features = "src/test/resources",
+	plugin = "json:target/cucumber-json-report.json"
+)
+
+public class RunCukeTest {
+	
+}
